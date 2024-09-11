b179ef887872c8134ca3f5802e8e085453ca41df
Brett Bodette <shinji.144@hotmail.com>
2024-09-10 19:39:47 -0500

    added webviewer webassembly project

---
+++ WebViewer/WebViewer.Client/Pages/ServerViewer.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace WebViewer.Client.Pages;
+
+public partial class ServerViewer
+{
+    private List<IPEndPoint> _servers = new();
+    private async Task RefreshList()
+    {
+        this._servers = (await QueryLib.MasterServerClient.GetServersList()).ToList();
+    }
+}
