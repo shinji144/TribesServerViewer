21c686aac947142c9d8b7c6b4463f4a93389ab22
Brett Bodette <shinji.144@hotmail.com>
2024-11-15 00:04:35 -0600

    UPDATE: migrated to newer aspire, and .net 9

--- WebViewer/WebViewer.AppHost/Program.cs
+++ WebViewer/WebViewer.AppHost/Program.cs
@@ -2,6 +2,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
+var db = builder.AddPostgres("postgres")
+    .WithDataVolume()
+    .AddDatabase("dbtest");
+
 var apiService = builder.AddProject<Projects.WebViewer_ApiService>("apiservice");
 
 builder.AddProject<Projects.WebViewer_Web>("webfrontend")
